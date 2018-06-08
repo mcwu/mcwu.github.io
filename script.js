c951aac06ed3cf78c8b9c7871483e38860ec0952
Mark Wu <mwu@qualtrics.com>
2018-06-07 18:18:55 -0700

    yo

---
+++ script.js
@@ -0,0 +1,13 @@
+// Code goes here
+
+function showAdobeData() {
+  var element = document.createElement('div');
+  element.innerText = 'Marketing Coud Visitor id: ' + (s.visitor.getMarketingCloudVisitorID())
+  element.style.padding = '10px';
+  document.body.append(element)
+}
+
+function register() {
+  alert("user registerd")
+}
+
